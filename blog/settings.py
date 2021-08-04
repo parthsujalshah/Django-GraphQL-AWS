80b2d610963f5bface52ab6fe3d6d0f17f0a4cf8
parthsujalshah <parthsujalshah@gmail.com>
2021-08-04 19:14:54 +0530

    print remove

--- blog/settings.py
+++ blog/settings.py
@@ -17,7 +17,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 env = environ.Env()
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
