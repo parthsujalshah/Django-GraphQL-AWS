05e9c11aaa2ed28362b3d3d3f7904e847e9d9764
parthsujalshah <parthsujalshah@gmail.com>
2021-08-05 16:03:44 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div style={{alignItems: "center", justifyContent: "center", display: "flex", flex: 1}}>
-      <Login />
+      <Register />
     </div>
   );
 }
