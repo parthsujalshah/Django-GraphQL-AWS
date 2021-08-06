ad4cb7c363995ba42a775738d84aae2f6a310b26
parthsujalshah <parthsujalshah@gmail.com>
2021-08-06 19:42:03 +0530

    new migrations and req

--- post/models.py
+++ post/models.py
@@ -5,12 +5,10 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
